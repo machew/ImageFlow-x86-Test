3b8fa141785fd4e32ad344e3fd437cdb7ee38ee7
Matthew Taylor <matthew@dealervision.com>
2021-01-12 10:44:31 -0800

    Add project files.

---
+++ ImageFlow-x86-Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using Imageflow.Fluent;
+using System;
+using System.Threading.Tasks;
+
+namespace ImageFlow_x86_Test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // From https://github.com/imazen/imageflow-dotnet#getting-image-dimensions-and-format
+            var imageBytes = Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEX/TQBcNTh/AAAAAXRSTlPM0jRW/QAAAApJREFUeJxjYgAAAAYAAzY3fKgAAAAASUVORK5CYII=");
+            var info = await ImageJob.GetImageInfo(new BytesSource(imageBytes));
+
+            Console.WriteLine(info.ImageWidth);
+            Console.Read();
+        }
+    }
+}
